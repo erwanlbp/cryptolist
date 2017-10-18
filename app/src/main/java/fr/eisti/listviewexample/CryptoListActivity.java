b2ebdf09289f4249e9c262d70fdf0fa4df55844f
erwanlbp <erwan.lbp@gmail.com>
2017-10-18 14:51:11 +0200

    Add main activity and CryptoListActivity

---
+++ app/src/main/java/fr/eisti/listviewexample/CryptoListActivity.java
@@ -0,0 +1,13 @@
+package fr.eisti.listviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CryptoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto_list);
+    }
+}
